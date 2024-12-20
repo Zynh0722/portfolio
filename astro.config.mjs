5dbc07915eeaa063c0fd65b68ce042a8372bf756
Zynh Ludwig <zynh0722@gmail.com>
2024-12-19 23:39:45 -0800

    add remark-gfm

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,19 +4,15 @@ import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import robotsTxt from "astro-robots-txt";
 import compress from "astro-compress";
+import gfm from "remark-gfm";
 
 import readTimePlugin from "./src/lib/readTime.mjs";
 
+const remarkPlugins = [readTimePlugin, gfm];
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [
-    mdx({ remarkPlugins: [readTimePlugin] }),
-    sitemap(),
-    compress(),
-    robotsTxt(),
-  ],
-  markdown: {
-    remarkPlugins: [readTimePlugin],
-  },
+  integrations: [mdx({ remarkPlugins }), sitemap(), compress(), robotsTxt()],
+  markdown: { remarkPlugins },
   site: "https://zynh.me/",
 });
